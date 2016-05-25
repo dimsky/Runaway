420aaf863c9ddc74532f485b79e3b445d435af2b
dimsky <dimsky@163.com>
2016-05-25 11:35:25 +0800

    add wechat sports hook

--- Runaway/Runaway.mm
+++ Runaway/Runaway.mm
@@ -1,89 +1,104 @@
 //
 //  Runaway.mm
-//  Runaway
+//  Runaway.mm
 //
-//  Created by dimsky on 16/5/20.
-//  Copyright (c) 2016年 __MyCompanyName__. All rights reserved.
+//  Created by dimsky on 16/5/19.
+//  Copyright (c) 2016年 songtaste. All rights reserved.
 //
 
 // CaptainHook by Ryan Petrich
 // see https://github.com/rpetrich/CaptainHook/
 
 #import <Foundation/Foundation.h>
-#import "CaptainHook/CaptainHook.h"
-#include <notify.h> // not required; for examples only
+#import "CaptainHook.h"
 
-// Objective-C runtime hooking using CaptainHook:
-//   1. declare class using CHDeclareClass()
-//   2. load class using CHLoadClass() or CHLoadLateClass() in CHConstructor
-//   3. hook method using CHOptimizedMethod()
-//   4. register hook using CHHook() in CHConstructor
-//   5. (optionally) call old method using CHSuper()
+/**
+ *  修改微信步数#52000
+ **/
 
+static int StepCount = 1234;
+static NSString *StepCountKey = @"StepCount";
+static NSString *HookSettingsFile = @"HookSettings.txt";
 
-@interface Runaway : NSObject
+CHDeclareClass(WCDeviceStepObject)
 
-@end
-
-@implementation Runaway
-
--(id)init
-{
-	if ((self = [super init]))
-	{
-	}
-
-    return self;
+//宏格式：参数的个数，返回值的类型，类的名称，selector的名称，selector的类型，selector对应的参数的变量名。
+CHMethod(0, unsigned int, WCDeviceStepObject, m7StepCount) {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *docDir = [paths objectAtIndex:0];
+    if (!docDir){ return StepCount;}
+    NSString *path = [docDir stringByAppendingPathComponent:HookSettingsFile];
+    NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithContentsOfFile:path];
+    int value = ((NSNumber *)dict[StepCountKey]).intValue;
+    return value;
 }
 
-@end
+CHDeclareClass(CMessageMgr);
 
+CHMethod(2, void, CMessageMgr, AsyncOnAddMsg, id, arg1, MsgWrap, id, arg2) {
+    CHSuper(2, CMessageMgr, AsyncOnAddMsg, arg1, MsgWrap, arg2);
+    Ivar uiMessageTypeIvar = class_getInstanceVariable(objc_getClass("CMessageWrap"), "m_uiMessageType");
+    ptrdiff_t offset = ivar_getOffset(uiMessageTypeIvar);
+    unsigned char *stuffBytes = (unsigned char *)(__bridge void *)arg2;
+    NSUInteger m_uiMessageType = * ((NSUInteger *)(stuffBytes + offset));
 
-@class ClassToHook;
+    Ivar nsFromUsrIvar = class_getInstanceVariable(objc_getClass("CMessageWrap"), "m_nsFromUsr");
+    id m_nsFromUsr = object_getIvar(arg2, nsFromUsrIvar);
 
-CHDeclareClass(ClassToHook); // declare class
+    Ivar nsContentIvar = class_getInstanceVariable(objc_getClass("CMessageWrap"), "m_nsContent");
+    id m_nsContent = object_getIvar(arg2, nsContentIvar);
 
-CHOptimizedMethod(0, self, void, ClassToHook, messageName) // hook method (with no arguments and no return value)
-{
-	// write code here ...
-	
-	CHSuper(0, ClassToHook, messageName); // call old (original) method
+    if (m_uiMessageType == 1) {
+        //普通消息
+
+        //微信的服务中心
+        Method methodMMServiceCenter = class_getClassMethod(objc_getClass("MMServiceCenter"), @selector(defaultCenter));
+        IMP impMMSC = method_getImplementation(methodMMServiceCenter);
+        id MMServiceCenter = impMMSC(objc_getClass("MMServiceCenter"), @selector(defaultCenter));
+        //通讯录管理器
+        id contactManager = ((id (*)(id, SEL, Class))objc_msgSend)(MMServiceCenter, @selector(getService:),objc_getClass("CContactMgr"));
+        id selfContact = objc_msgSend(contactManager, @selector(getSelfContact));
+
+        Ivar nsUsrNameIvar = class_getInstanceVariable([selfContact class], "m_nsUsrName");
+        id m_nsUsrName = object_getIvar(selfContact, nsUsrNameIvar);
+        BOOL isMesasgeFromMe = NO;
+        if ([m_nsFromUsr isEqualToString:m_nsUsrName]) {
+            //发给自己的消息
+            isMesasgeFromMe = YES;
+        }
+
+        if (isMesasgeFromMe)
+        {
+            if ([m_nsContent rangeOfString:@"修改微信步数#"].location != NSNotFound) {
+                NSArray *array = [m_nsContent componentsSeparatedByString:@"#"];
+                if (array.count == 2) {
+                    StepCount = ((NSNumber *)array[1]).intValue;
+
+                    // save to file
+                    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+                    NSString *docDir = [paths objectAtIndex:0];
+                    if (!docDir){ return;}
+                    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+                    NSString *path = [docDir stringByAppendingPathComponent:HookSettingsFile];
+                    dict[StepCountKey] = [NSNumber numberWithInt:StepCount];
+                    [dict writeToFile:path atomically:YES];
+
+                    NSLog(@"微信步数已修改为 : %d", StepCount);
+                }
+            }
+        }
+    }
 }
 
-CHOptimizedMethod(2, self, BOOL, ClassToHook, arg1, NSString*, value1, arg2, BOOL, value2) // hook method (with 2 arguments and a return value)
-{
-	// write code here ...
 
-	return CHSuper(2, ClassToHook, arg1, value1, arg2, value2); // call old (original) method and return its return value
+__attribute__((constructor)) static void entry() {
+    NSLog(@"微信步数燥起来!");
+    CHLoadLateClass(WCDeviceStepObject);
+    CHClassHook(0, WCDeviceStepObject,m7StepCount);
+
+    CHLoadLateClass(CMessageMgr);
+    CHClassHook(2, CMessageMgr, AsyncOnAddMsg, MsgWrap);
 }
 
-static void WillEnterForeground(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
-{
-	// not required; for example only
-}
 
-static void ExternallyPostedNotification(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
-{
-	// not required; for example only
-}
 
-CHConstructor // code block that runs immediately upon load
-{
-	@autoreleasepool
-	{
-		// listen for local notification (not required; for example only)
-		CFNotificationCenterRef center = CFNotificationCenterGetLocalCenter();
-		CFNotificationCenterAddObserver(center, NULL, WillEnterForeground, CFSTR("UIApplicationWillEnterForegroundNotification"), NULL, CFNotificationSuspensionBehaviorCoalesce);
-		
-		// listen for system-side notification (not required; for example only)
-		// this would be posted using: notify_post("dimsky.Runaway.eventname");
-		CFNotificationCenterRef darwin = CFNotificationCenterGetDarwinNotifyCenter();
-		CFNotificationCenterAddObserver(darwin, NULL, ExternallyPostedNotification, CFSTR("dimsky.Runaway.eventname"), NULL, CFNotificationSuspensionBehaviorCoalesce);
-		
-		// CHLoadClass(ClassToHook); // load class (that is "available now")
-		// CHLoadLateClass(ClassToHook);  // load class (that will be "available later")
-		
-		CHHook(0, ClassToHook, messageName); // register hook
-		CHHook(2, ClassToHook, arg1, arg2); // register hook
-	}
-}
